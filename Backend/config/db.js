bb00c4a3bc935fe16e8bb55c17acf1d0aa6e0314
ashwiniprakash <ashwini23bce@gmail.com>
2024-06-21 19:09:10 +0530

    latest

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,12 +1,7 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
 
-const connection = mongoose.connect(process.env.MONGO_URL,  {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 5000, 
-  socketTimeoutMS: 30000,
-})
+const connection = mongoose.connect(process.env.MONGO_URL)
 module.exports = {
   connection
 };
